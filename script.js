923edeb1f950a78525c0938ea4ac96a52bfb000b
aveencoder <aveen598@gmail.com>
2025-08-11 22:05:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,50 @@
-//complete this code
 class Person {
+  private _name: string;
+  private _age: number;
+
   constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+    this._name = name;
+    this._age = age;
   }
 
   // Getter for name
   get name(): string {
-    return this.name;
+    return this._name;
   }
 
   // Setter for age
   set age(age: number) {
-    this.age = age;
-  }}
+    this._age = age;
+  }
+
+  // Optional: Getter for age (useful for tests)
+  get age(): number {
+    return this._age;
+  }
+}
 
 class Student extends Person {
-
-	constructor(name: string, age: number) {
+  constructor(name: string, age: number) {
     super(name, age);
-	}
-	study(): void{
-		console.log(`${this.name} is studying`)
-	}
+  }
+
+  study(): void {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-   constructor(name: string, age: number) {
+  constructor(name: string, age: number) {
     super(name, age);
-	}
-	teach(): void{
-		console.log(`${this.name} is teaching`)
-	}
+  }
+
+  teach(): void {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
