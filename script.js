d64e3056ab33c9386e63fccec23cfc286bb15d63
aveencoder <aveen598@gmail.com>
2025-08-11 21:58:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 //complete this code
-class Person {}
+class Person {
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name(): string {
+    return this.name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age: number) {
+    this.age = age;
+  }}
+
+class Student extends Person {
+
+	constructor(name: string, age: number) {
+    super(name, age);
+	}
+	study(): void{
+		console.log(`${this.name} is studying`)
+	}
+}
+
+class Teacher extends Person {
+   constructor(name: string, age: number) {
+    super(name, age);
+	}
+	teach(): void{
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
